2eede0245a34fb14d76400167878f19eaa39012b
Haad <haaddev@gmail.com>
2025-03-04 17:19:07 +0500

    done

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './Dash.css'
+import "./Dash.css";
 
 function Dash() {
   let [userinp, setUserinp] = useState("");
@@ -11,31 +11,42 @@ function Dash() {
     todos.push(userinp);
     setUserinp("");
   }
-  
-  
-  
-  
-  
-  
-  
-  
+
   return (
     <>
       <h1>Todo App</h1>
       <form className="form">
         <input
-        className="title input"
+          className="title input"
           type="text"
           value={userinp}
           onChange={(e) => setUserinp(e.target.value)}
         />
-        <button className="add-btn" onClick={addtodo}>click</button>
+        <button className="add-btn" onClick={addtodo}>
+          click
+        </button>
       </form>
-      <ul className="todo-list">
-        {todos.map((todo) => (
-          <li className="todo-item">{todo} <span><button>delete</button><button>edit</button></span></li>
-        ))}
-      </ul>
+      <div>
+        {todos.length > 0 ? (
+          todos.map((item, index) => {
+            return (
+              <>
+                <ul className="todo-list">
+                  <li className="todo-item">
+                    {item}
+                    <span>
+                      <button>delete</button>
+                      <button>edit</button>
+                    </span>
+                  </li>
+                </ul>
+              </>
+            );
+          })
+        ) : (
+          <h1 style={{ marginTop: "40px" }}>Nothing To See...</h1>
+        )}
+      </div>
     </>
   );
 }
