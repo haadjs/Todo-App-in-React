0e9bf2be9cd01fc6eb9a63dbe561ab2e4534eb4b
Haad <haaddev@gmail.com>
2025-03-04 17:02:01 +0500

    done

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -1,19 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import './Dash.css'
 
 function Dash() {
-  let [userinp, setUserinp] = useState('');
+  let [userinp, setUserinp] = useState("");
+  let [todos, settodo] = useState([]);
 
-  function addtodo () {
-    setUserinp(userinp)
-    console.log(userinp);
-    setUserinp('')
-    
+  function addtodo(e) {
+    e.preventDefault();
+    setUserinp(userinp);
+    todos.push(userinp);
+    setUserinp("");
   }
+  
+  
+  
+  
+  
+  
+  
+  
   return (
     <>
       <h1>Todo App</h1>
-      <input type="text" value={userinp} onChange={(e)=> setUserinp(e.target.value)} />
-      <button onClick={addtodo}>click</button>
+      <form className="form">
+        <input
+        className="title input"
+          type="text"
+          value={userinp}
+          onChange={(e) => setUserinp(e.target.value)}
+        />
+        <button className="add-btn" onClick={addtodo}>click</button>
+      </form>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li className="todo-item">{todo} <span><button>delete</button><button>edit</button></span></li>
+        ))}
+      </ul>
     </>
   );
 }
