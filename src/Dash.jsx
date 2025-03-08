da3e0a009f3a1484772c20192674805c8004f24b
Haad <haaddev@gmail.com>
2025-03-08 05:26:27 +0500

    done

--- src/Dash.jsx
+++ src/Dash.jsx
@@ -2,47 +2,59 @@ import React, { useState } from "react";
 import "./Dash.css";
 
 function Dash() {
-  let [userinp, setUserinp] = useState("");
-  let [todos, settodo] = useState([]);
+  const [userinp, setUserinp] = useState("");
+  const [todos, settodo] = useState([]);
 
   function addtodo(e) {
     e.preventDefault();
-    setUserinp(userinp);
-    todos.push(userinp);
+    if (userinp.trim() === "") return;
+    settodo([...todos, userinp]);
     setUserinp("");
+  }
+
+  function delTodo(index) {
+    todos.splice(index, 1);
+    settodo([...todos]);
+  }
+
+  function editTodo(index) {
+    let updatVal = prompt("enter a new value");
+    if (!updatVal) {
+      alert("enter a val");
+      return;
+    }
+
+    todos[index] = updatVal;
+    settodo([...todos]);
   }
 
   return (
     <>
       <h1>Todo App</h1>
-      <form className="form">
+      <form className="form" onSubmit={addtodo}>
         <input
           className="title input"
           type="text"
           value={userinp}
           onChange={(e) => setUserinp(e.target.value)}
         />
-        <button className="add-btn" onClick={addtodo}>
-          click
+        <button className="add-btn" type="submit">
+          Add Todo
         </button>
       </form>
       <div>
         {todos.length > 0 ? (
-          todos.map((item, index) => {
-            return (
-              <>
-                <ul className="todo-list">
-                  <li className="todo-item">
-                    {item}
-                    <span>
-                      <button>delete</button>
-                      <button>edit</button>
-                    </span>
-                  </li>
-                </ul>
-              </>
-            );
-          })
+          <ul className="todo-list">
+            {todos.map((item, index) => (
+              <li className="todo-item" key={index}>
+                {item}
+                <span>
+                  <button onClick={() => delTodo(index)}>Delete</button>
+                  <button onClick={() => editTodo(index)}>Edit</button>
+                </span>
+              </li>
+            ))}
+          </ul>
         ) : (
           <h1 style={{ marginTop: "40px" }}>Nothing To See...</h1>
         )}
